014fb2ba5a9019b55d7c4afb0562940ac0349800
Jackson Xu <jacksonxu3@gmail.com>
2023-01-18 21:30:00 -0800

    Create SortedSquaredArray.py

---
+++ SortedSquaredArray.py
@@ -0,0 +1,15 @@
+# Sorted Squared Array
+
+'''
+Write a function that takes in a non-empty array of integers that are sorted in ascending order, and 
+returns a new array of the same length with the squares of the original integers also sorted. 
+'''
+
+# Quick and Short Solution, O(nlog x)
+def sortedSquaredArray(array):
+    # List comprehension
+    unsortedSquares = [x*x for x in array]
+    unsortedSquares.sort()
+    return unsortedSquares
+  
+# Optimal Solution
