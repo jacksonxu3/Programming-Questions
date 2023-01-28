6fc1a45a918994f960f1b377d7a6299d44e287a0
Jackson Xu <jacksonxu3@gmail.com>
2023-01-28 01:15:45 -0800

    Update NonConstructibleChange.py

--- NonConstructibleChange.py
+++ NonConstructibleChange.py
@@ -2,7 +2,7 @@
 
 '''
 Given an array of positive integers, determine the smallest integer 
-that cannot be created by summing any amount of the given integers. 
+that cannot be created by summing any amount of the given integers.
 '''
 
 # Optimal solution, O(n log n)
